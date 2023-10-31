41cfdcc8947d4ab24309d1fa889dfa1b43f2ac96
Laurenz Ruzicka <Laurenz.Ruzicka@ait.ac.at>
2023-10-31 11:52:46 +0100

    Made prompts more specific for Elasticsearch

--- libs/langchain/langchain/tools/elasticsearch_database/prompt.py
+++ libs/langchain/langchain/tools/elasticsearch_database/prompt.py
@@ -1,18 +1,16 @@
 # flake8: noqa
 QUERY_CHECKER = """
 {query}
-Double check the query for index {index} above for common mistakes, including:
+Double check the search above for index {index} for common mistakes, including:
 - Missing or mismatched curly braces.
 - Typos or incorrect field names in the query DSL.
 - The wildcard query is not supported in a "bool" query's filter context.
 - Elasticsearch queries need to follow a specific structure, and incorrect nesting or order can lead to errors.
 - Attempting to search for data with a query of a different data type.
 - Using the correct number of arguments for functions.
-- Casting to the correct data type.
-- When field names in the query are ambiguous and could refer to multiple fields.
 
-If there are any of the above mistakes, rewrite the query. If there are no mistakes, just reproduce the original query.
+If there are any of the above mistakes, rewrite the search. If there are no mistakes, just reproduce the original search.
 
-Output the final Elasticsearch query only.
+Output the final Elasticsearch search only.
 
-Elasticsearch Query: """
+Elasticsearch search: """
