deaf296c074807c1706333ff849528aa3c5da27f
Laurenz Ruzicka <Laurenz.Ruzicka@ait.ac.at>
2023-11-06 13:05:38 +0100

    Extended prompt

--- libs/langchain/langchain/agents/agent_toolkits/elasticsearch/prompt.py
+++ libs/langchain/langchain/agents/agent_toolkits/elasticsearch/prompt.py
@@ -13,7 +13,7 @@ If the question does not seem related to the database, just return "I don't know
 ELASTICSEARCH_SUFFIX = """Begin!
 
 Question: {input}
-Thought: I should look at the indices in the Elasticsearch database to see what I can search for.  I should also look at the schema of the best fitting index to see what fields I can user in the search.
+Thought: I should look at the indices in the Elasticsearch database.  I should also look at the mapping of the best fitting index to see what fields I can user in the search and I should remember the relevant fields of that mapping.
 {agent_scratchpad}"""
 
 ELASTICSEARCH_FUNCTIONS_SUFFIX = """I should look at the indices and their schemes in the database to see what I can search for."""
